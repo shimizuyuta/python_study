72d585645b9a39d255f3b32a39885dc6bb6befc8
clean_water <shmizuyuta213@icloud.com>
2022-03-06 06:42:09 +0900

    リファクタリング

--- test.py
+++ test.py
@@ -4,26 +4,23 @@ import time
 import pandas as pd 
 from bs4 import BeautifulSoup
 import csv
-
+from selenium.webdriver.common.keys import Keys
 
 CHROMEDRIVER ="C:\\Users\\clean_water\\Downloads\\chromedriver_win32\\chromedriver.exe"
 browser = webdriver.Chrome(executable_path=CHROMEDRIVER)
 google = 'https://www.google.com/'
 google_img = 'https://www.google.co.jp/imghp?hl=ja&tab=wi&ogbl'
 
+
 def export_to_csv():
-  browser.get(google_img)
+  browser.get(google)
   print('search google')
+  search = browser.find_element_by_name('q').send_keys('東京都',Keys.ENTER)
   time.sleep(1)
-  search = browser.find_element_by_name('q')
-  search.send_keys('')
-  time.sleep(1)
-  search.submit()
   titles = []
 
   try:
     elms = browser.find_elements_by_xpath("//h3")
-    print('success')
 
     for element in elms:
       titles.append([element.text])
@@ -36,7 +33,7 @@ def export_to_csv():
     print('error')
     browser.close()
 
-  time.sleep(2)
+  time.sleep(1)
   browser.quit()
 
 export_to_csv()
